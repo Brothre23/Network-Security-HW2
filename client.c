99c5f2d298d8fceaf867433e73c2e80ef0e364ea
Brothre23 <daveyu824@gmail.com>
2020-04-26 14:25:53 +0800

    server and client exchanged

--- client.c
+++ client.c
@@ -1,9 +1,3 @@
-/*File:client.c
- *Auth:sjin
- *Date：2014-03-11
- *
- */
-
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -23,9 +17,9 @@
 
 int main(int argc, char **argv)
 {
-    int sock_fd, send_len, read_len, fd;
+    int sock_fd, len, fd;
     char file_name[50];
-    char full_name[50] = "./test_file/";
+    char full_name[50] = "./new_file/";
     struct sockaddr_in dest;
     char buffer[MAXBUF];
     SSL_CTX *ctx;
@@ -37,37 +31,35 @@ int main(int argc, char **argv)
         exit(0);
     }
 
-    /* SSL 庫初始化 */
+    mkdir("./new_file", S_IRWXU);
+
     SSL_library_init();
     OpenSSL_add_all_algorithms();
     SSL_load_error_strings();
+
     ctx = SSL_CTX_new(SSLv23_client_method());
     if (ctx == NULL)
     {
         ERR_print_errors_fp(stdout);
         exit(1);
     }
-    /* 載入使用者的數字證書， 此證書用來發送給客戶端。 證書裡包含有公鑰 */
+
     if (SSL_CTX_use_certificate_file(ctx, "./CA/server.crt", SSL_FILETYPE_PEM) <= 0)
     {
         ERR_print_errors_fp(stdout);
         exit(1);
     }
-
-    /* 載入使用者私鑰 */
     if (SSL_CTX_use_PrivateKey_file(ctx, "./CA/server.key", SSL_FILETYPE_PEM) <= 0)
     {
         ERR_print_errors_fp(stdout);
         exit(1);
     }
-    /* 檢查使用者私鑰是否正確 */
     if (!SSL_CTX_check_private_key(ctx))
     {
         ERR_print_errors_fp(stdout);
         exit(1);
     }
 
-    /* 建立一個 socket 用於 tcp 通訊 */
     if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("socket ");
@@ -75,7 +67,6 @@ int main(int argc, char **argv)
     }
     printf("socket created\n");
 
-    /* 初始化伺服器端（對方）的地址和埠資訊 */
     bzero(&dest, sizeof(dest));
     dest.sin_family = AF_INET;
     dest.sin_port = htons(atoi(argv[2]));
@@ -86,7 +77,6 @@ int main(int argc, char **argv)
     }
     printf("address created\n");
 
-    /* 連線伺服器 */
     if (connect(sock_fd, (struct sockaddr *)&dest, sizeof(dest))!= 0)
     {
         perror("connect ");
@@ -94,17 +84,16 @@ int main(int argc, char **argv)
     }
     printf("server connected\n");
 
-    /* 基於 ctx 產生一個新的 SSL */
     ssl = SSL_new(ctx);
     SSL_set_fd(ssl, sock_fd);
-    /* 建立 SSL 連線 */
+
     if (SSL_connect(ssl) == -1)
         ERR_print_errors_fp(stderr);
     else
         printf("connected with %s encryption\n", SSL_get_cipher(ssl));
 
     printf("\navailable file list:\n");
-    /* 印出檔名 */
+
     while (1)
     {
         SSL_read(ssl, buffer, MAXBUF);
@@ -114,48 +103,47 @@ int main(int argc, char **argv)
         bzero(buffer, MAXBUF);
     }
 
-    /* 接收使用者輸入的檔名，並開啟檔案 */
     printf("\nplease input the name of the file you want to load:\n>");
     scanf("%s", file_name);
 
+    if (SSL_write(ssl, file_name, strlen(file_name)) < 0)
+        printf("'%s'message send failure! error code is %d, error messages are '%s'\n", buffer, errno, strerror(errno));
     strcat(full_name, file_name);
 
-    if ((fd = open(full_name, O_RDONLY, 0666)) < 0)
+    if ((fd = open(full_name, O_CREAT | O_RDWR, 0666)) < 0)
     {
         perror("open ");
         exit(1);
     }
 
-    send_len = SSL_write(ssl, file_name, strlen(file_name));
-    if (send_len < 0)
-        printf("'%s'message send failure! error code is %d, error messages are '%s'\n", buffer, errno, strerror(errno));
-
-    /* 迴圈傳送檔案內容到伺服器 */
     bzero(buffer, MAXBUF);
-    while ((read_len = read(fd, buffer, MAXBUF)))
+
+    while (1)
     {
-        if (read_len < 0)
+        len = SSL_read(ssl, buffer, MAXBUF);
+
+        if ((int)len < 0)
         {
-            perror("read ");
+            printf("failed to receive message! error code is %d, error messages are '%s'\n", errno, strerror(errno));
             exit(1);
         }
-        else
+        if (write(fd, buffer, len) < 0)
         {
-            send_len = SSL_write(ssl, buffer, read_len);
-            if (send_len < 0)
-                printf("'%s'message send failure! error code is %d, error messages are '%s'\n", buffer, errno, strerror(errno));
+            perror("write ");
+            exit(1);
         }
-        bzero(buffer, MAXBUF);
-    }
-    printf("\nsend complete!\n");
-    close(fd);
 
-    sleep(5);
-    /* 關閉連線 */
+        if (len < MAXBUF)
+        {
+            printf("receive complete!\n\n");
+            break;
+        }
+    }
+
+    close(fd);
     SSL_shutdown(ssl);
     SSL_free(ssl);
     close(sock_fd);
-    SSL_CTX_free(ctx);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -1,8 +1,3 @@
-/*File:server.c
- *Auth:sjin
- *Date：2014-03-11
- *
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -24,17 +19,14 @@
 
 int main(int argc, char **argv)
 {
-    int server_fd, client_fd, fd;
-    socklen_t len;
+    int server_fd, client_fd, fd, read_len, send_len;
+    socklen_t sock_len;
     struct sockaddr_in server_addr, client_addr;
     char buffer[MAXBUF];
-    char new_file_name[50] = "./new_file/";
+    char file_name[50] = "./test_file/";
     SSL_CTX *ctx;
     DIR *dir;
     struct dirent *entry;
-
-    /* 在根目錄下建立一個newfile資料夾 */
-    mkdir("./new_file", S_IRWXU);
 
     if (argc != 2)
     {
@@ -42,53 +34,44 @@ int main(int argc, char **argv)
         exit(0);
     }
 
-    /* SSL 庫初始化 */
     SSL_library_init();
-    /* 載入所有 SSL 演算法 */
     OpenSSL_add_all_algorithms();
-    /* 載入所有 SSL 錯誤訊息 */
     SSL_load_error_strings();
-    /* 以 SSL V2 和 V3 標準相容方式產生一個 SSL_CTX ，即 SSL Content Text */
+
     ctx = SSL_CTX_new(SSLv23_server_method());
-    /* 也可以用 SSLv2_server_method() 或 SSLv3_server_method() 單獨表示 V2 或 V3標準 */
     if (ctx == NULL)
     {
         ERR_print_errors_fp(stdout);
         exit(1);
     }
-    /* 載入使用者的數字證書， 此證書用來發送給客戶端。 證書裡包含有公鑰 */
+
     if (SSL_CTX_use_certificate_file(ctx, "./CA/client.crt", SSL_FILETYPE_PEM) <= 0)
     {
         ERR_print_errors_fp(stdout);
         exit(1);
     }
-
-    /* 載入使用者私鑰 */
     if (SSL_CTX_use_PrivateKey_file(ctx, "./CA/client.key", SSL_FILETYPE_PEM) <= 0)
     {
         ERR_print_errors_fp(stdout);
         exit(1);
     }
-    /* 檢查使用者私鑰是否正確 */
     if (!SSL_CTX_check_private_key(ctx))
     {
         ERR_print_errors_fp(stdout);
         exit(1);
     }
 
-    /* 開啟一個 socket 監聽 */
     if ((server_fd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
     {
         perror("socket ");
         exit(1);
     }
-    else
-        printf("socket created\n");
+    printf("socket created\n");
 
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, NULL, sizeof(int));
 
     bzero(&server_addr, sizeof(server_addr));
-    server_addr.sin_family = PF_INET;
+    server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(atoi(argv[1]));
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
@@ -97,35 +80,30 @@ int main(int argc, char **argv)
         perror("bind ");
         exit(1);
     }
-    else
-        printf("binded\n");
+    printf("binded\n");
 
     if (listen(server_fd, 1) == -1)
     {
         perror("listen ");
         exit(1);
     }
-    else
-        printf("begin listen\n\n");
+    printf("begin listen\n\n");
 
     while (1)
     {
         SSL *ssl;
-        len = sizeof(struct sockaddr);
-        /* 等待客戶端連上來 */
-        if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &len)) == -1)
+        sock_len = sizeof(struct sockaddr);
+
+        if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &sock_len)) == -1)
         {
             perror("accept ");
             exit(errno);
         }
-        else
-            printf("server: got connection from %s, port %d, socket %d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), client_fd);
+        printf("server: got connection from %s, port %d, socket %d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), client_fd);
 
-        /* 基於 ctx 產生一個新的 SSL */
         ssl = SSL_new(ctx);
-        /* 將連線使用者的 socket 加入到 SSL */
         SSL_set_fd(ssl, client_fd);
-        /* 建立 SSL 連線 */
+
         if (SSL_accept(ssl) == -1)
         {
             perror("accept ");
@@ -135,7 +113,6 @@ int main(int argc, char **argv)
 
         if ((dir = opendir("./test_file/")) != NULL)
         {
-            /* print all the files and directories within directory */
             while ((entry = readdir(dir)) != NULL)
                 SSL_write(ssl, entry->d_name, strlen(entry->d_name));
             closedir(dir);
@@ -144,59 +121,42 @@ int main(int argc, char **argv)
         else
             perror("open directory ");
 
-        /* 接受客戶端所傳檔案的檔名並在特定目錄建立空檔案 */
-        bzero(buffer, MAXBUF);
-        bzero(new_file_name + 11, 39);
-        len = SSL_read(ssl, buffer, MAXBUF);
-
-        if (len == 0)
-            printf("receive complete! \n");
-        else if (len < 0)
+        if (SSL_read(ssl, buffer, MAXBUF) < 0)
             printf("failed to receive message! error code is %d, error messages are '%s'\n", errno, strerror(errno));
-        strcat(new_file_name, buffer);
+        bzero(file_name + 12, 38);
+        strcat(file_name, buffer);
 
-        if ((fd = open(new_file_name, O_CREAT | O_RDWR, 0666)) < 0)
+        if ((fd = open(file_name, O_RDONLY, 0666)) < 0)
         {
             perror("open ");
             exit(1);
         }
 
         bzero(buffer, MAXBUF);
-        /* 接收客戶端的資料並寫入檔案 */
-        while (1)
+        while ((read_len = read(fd, buffer, MAXBUF)))
         {
-            len = SSL_read(ssl, buffer, MAXBUF);
-
-            if ((int)len < 0)
+            if (read_len < 0)
             {
-                printf("failed to receive message! error code is %d, error messages are '%s'\n", errno, strerror(errno));
+                perror("read ");
                 exit(1);
             }
-            if (write(fd, buffer, len) < 0)
+            else
             {
-                perror("write ");
-                exit(1);
+                send_len = SSL_write(ssl, buffer, read_len);
+                if (send_len < 0)
+                    printf("'%s'message send failure! error code is %d, error messages are '%s'\n", buffer, errno, strerror(errno));
             }
-
-            if (len < MAXBUF)
-            {
-                printf("receive complete!\n\n");
-                break;
-            }
+            bzero(buffer, MAXBUF);
         }
-        /* 關閉檔案 */
+        printf("send complete!\n\n");
+        
         close(fd);
-        /* 關閉 SSL 連線 */
         SSL_shutdown(ssl);
-        /* 釋放 SSL */
         SSL_free(ssl);
-        /* 關閉 socket */
         close(client_fd);
     }
 
-    /* 關閉監聽的 socket */
     close(server_fd);
-    /* 釋放 CTX */
     SSL_CTX_free(ctx);
     return 0;
 }
